97334f0610a255dd7a6b092c7399741ad8236ff6
Aryan <aryandineshsingh1@gmail.com>
2026-08-20 11:33:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes.forEach(function (input, index) {
+
+    // Typing
+    input.addEventListener("input", function () {
+
+        if (input.value !== "" && index < codes.length - 1) {
+            codes[index + 1].focus();
+        }
+
+    });
+
+    // Backspace
+    input.addEventListener("keydown", function (event) {
+
+        if (event.key === "Backspace") {
+
+            if (index > 0) {
+                codes[index - 1].focus();
+            }
+
+        }
+
+    });
+
+});
